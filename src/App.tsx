cd992baef51ff518b08d782d543c7b1806eac9e0
Semicoln Tech <web@semicoln.com>
2024-08-01 15:37:35 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
-import { Counter } from "./components/Counter";
-import { Jetton } from "./components/Jetton";
-import { TransferTon } from "./components/TransferTon";
+// import { Counter } from "./components/Counter";
+// import { Jetton } from "./components/Jetton";
+// import { TransferTon } from "./components/TransferTon";
 import styled from "styled-components";
 import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
@@ -34,14 +34,14 @@ function App() {
       <AppContainer>
         <FlexBoxCol>
           <FlexBoxRow>
-            {/* <TonConnectButton />
+            <TonConnectButton />
             <Button>
               {network
                 ? network === CHAIN.MAINNET
                   ? "mainnet"
                   : "testnet"
                 : "N/A"}
-            </Button> */}
+            </Button>
           </FlexBoxRow>
           {/* <Counter />
           <TransferTon />
